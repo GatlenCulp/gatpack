f247b34bdd2638d3e3edd32f3187949f0c048fd4
GatlenCulp <GatlenCulp@gmail.com>
2025-02-05 00:27:26 -0500

    chore: update development dependencies and configuration

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,10 +1,7 @@
 #! /usr/bin/env bash
 
 # Install Dependencies
-uv sync
-uv pip install -r dev-requirements.txt
-
+uv venv
+uv sync --dev
 source './.venv/bin/activate'
-
-# Install pre-commit hooks
-uv run pre-commit install --install-hooks
+pre-commit install --install-hooks
