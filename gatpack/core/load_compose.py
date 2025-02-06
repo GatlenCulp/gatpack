aa646a2a0fc004e7570db9bf5b24e1441f6512cc
GatlenCulp <GatlenCulp@gmail.com>
2025-02-06 18:17:04 -0500

    feat: refactor config to compose; add orjson dependency

--- gatpack/core/load_compose.py
+++ gatpack/core/load_compose.py
@@ -2,10 +2,13 @@
 
 from pathlib import Path
 from typing import Any
+from gatpack.models.GatPackCompose import GatPackCompose
 
 
 def load_config(
-    config: Path,
+    compose: Path,
     **kwargs: dict[str, Any],
 ) -> dict[str, Any]:
     """Loads the GatPack config file for rendering jinja and others."""
+    gp_compose = GatPackCompose.model_validate_json(compose.read_text())
+    return gp_compose
